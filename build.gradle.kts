9f97079c9f25996ca956bf1776376e1c8cb494a1
Andreas Fehn <fehnomenal@fehn.club>
2019-05-26 08:31:20 +0200

    Create new module to load factorio data

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.3.31"
 }
@@ -9,12 +7,4 @@ version = "0.1.0"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
